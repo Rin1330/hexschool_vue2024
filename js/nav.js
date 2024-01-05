b878f3901627178cdd59a4af6e280009094f2613
sakimotorin <sakimotorin@gmail.com>
2024-01-05 14:21:31 +0800

    HexSchool_Vue_2024 init - week1

---
+++ js/nav.js
@@ -0,0 +1,44 @@
+export const pages = [
+  {
+    title: '第一週 - 從函式拆解認識設計模式',
+    week: '第一週',
+    link: './week1.html',
+    is_active: true
+  },
+  {
+    title: '第二週 - ',
+    week: '第二週',
+    link: './week2.html',
+    is_active: false
+  },
+  {
+    title: '第三週 - ',
+    week: '第三週',
+    link: 'javascript:;',
+    is_active: false
+  },
+  {
+    title: '第四週 - ',
+    week: '第四週',
+    link: 'javascript:;',
+    is_active: false
+  },
+  {
+    title: '第五週 - ',
+    week: '第五週',
+    link: 'javascript:;',
+    is_active: false
+  },
+  {
+    title: '第六週 - ',
+    week: '第六週',
+    link: 'javascript:;',
+    is_active: false
+  },
+  {
+    title: '第七週 - ',
+    week: '第七週',
+    link: 'javascript:;',
+    is_active: false
+  }
+]

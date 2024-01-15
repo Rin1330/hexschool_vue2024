14d1329e4e4e4ecba05b044e9632f0df1243d1b4
sakimoto rin <sakimotorin@gmail.com>
2024-01-16 03:36:34 +0800

    update folder structure

--- js/nav.js
+++ js/nav.js
@@ -2,43 +2,19 @@ export const pages = [
   {
     title: '第一週 - 從函式拆解認識設計模式',
     week: '第一週',
-    link: './../week1/index.html',
+    link: './week1.html',
     is_active: true
   },
   {
     title: '第二週 - RESTful API 串接',
     week: '第二週',
-    link: './../week2/login.html',
+    link: './week2-login.html',
     is_active: true
   },
   {
     title: '第三週 - 熟練 Vue.js',
     week: '第三週',
-    link: '/week3/login.html',
+    link: './week3-login.html',
     is_active: false
   },
-  {
-    title: '第四週 - ',
-    week: '第四週',
-    link: '../week4/index.html',
-    is_active: false
-  },
-  {
-    title: '第五週 - ',
-    week: '第五週',
-    link: '../week5/index.html',
-    is_active: false
-  },
-  {
-    title: '第六週 - ',
-    week: '第六週',
-    link: '../week6/index.html',
-    is_active: false
-  },
-  {
-    title: '第七週 - ',
-    week: '第七週',
-    link: '../week/index.html',
-    is_active: false
-  }
 ]
